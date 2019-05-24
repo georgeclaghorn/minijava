b7e6d846a1b51c285fd95e19eb15c22beea6fd5b
George Claghorn <george@basecamp.com>
2019-05-23 21:23:09 -0400

    Remove unnecessary assignments

--- lib/minijava/lexer.rb
+++ lib/minijava/lexer.rb
@@ -29,109 +29,115 @@ module MiniJava
         @line += 1 if peek(1) == "\n"
 
         case
-        when text = scan(/class/)
+        when scan(/[ \r\n\t\f]+/)
+          nil
+
+        when scan(/\/\*.*?\*\/|\/\*\*+\/|\/\/[^\r\n]*/)
+          nil
+
+        when scan(/class/)
           emit :CLASS
 
-        when text = scan(/extends/)
+        when scan(/extends/)
           emit :EXTENDS
 
-        when text = scan(/public/)
+        when scan(/public/)
           emit :PUBLIC
 
-        when text = scan(/static/)
+        when scan(/static/)
           emit :STATIC
 
-        when text = scan(/void/)
+        when scan(/void/)
           emit :VOID
 
-        when text = scan(/main/)
+        when scan(/main/)
           emit :MAIN
 
-        when text = scan(/new/)
+        when scan(/new/)
           emit :NEW
 
-        when text = scan(/if/)
+        when scan(/if/)
           emit :IF
 
-        when text = scan(/else/)
+        when scan(/else/)
           emit :ELSE
 
-        when text = scan(/while/)
+        when scan(/while/)
           emit :WHILE
 
-        when text = scan(/length/)
+        when scan(/length/)
           emit :LENGTH
 
-        when text = scan(/true/)
+        when scan(/true/)
           emit :TRUE
 
-        when text = scan(/false/)
+        when scan(/false/)
           emit :FALSE
 
-        when text = scan(/return/)
+        when scan(/return/)
           emit :RETURN
 
-        when text = scan(/this/)
+        when scan(/this/)
           emit :THIS
 
-        when text = scan(/boolean/)
+        when scan(/boolean/)
           emit :BOOLEAN
 
-        when text = scan(/int/)
+        when scan(/int/)
           emit :INT
 
-        when text = scan(/String/)
+        when scan(/String/)
           emit :STRING
 
-        when text = scan(/System\.out\.println/)
+        when scan(/System\.out\.println/)
           emit :PRINTLN
 
-        when text = scan(/\{/)
+        when scan(/\{/)
           emit :LBRACE
 
-        when text = scan(/\}/)
+        when scan(/\}/)
           emit :RBRACE
 
-        when text = scan(/;/)
+        when scan(/;/)
           emit :SEMICOLON
 
-        when text = scan(/,/)
+        when scan(/,/)
           emit :COMMA
 
-        when text = scan(/\(/)
+        when scan(/\(/)
           emit :LPAREN
 
-        when text = scan(/\)/)
+        when scan(/\)/)
           emit :RPAREN
 
-        when text = scan(/\[/)
+        when scan(/\[/)
           emit :LBRACKET
 
-        when text = scan(/\]/)
+        when scan(/\]/)
           emit :RBRACKET
 
-        when text = scan(/\./)
+        when scan(/\./)
           emit :DOT
 
-        when text = scan(/\!/)
+        when scan(/\!/)
           emit :NOT
 
-        when text = scan(/\*/)
+        when scan(/\*/)
           emit :STAR
 
-        when text = scan(/\+/)
+        when scan(/\+/)
           emit :PLUS
 
-        when text = scan(/-/)
+        when scan(/-/)
           emit :MINUS
 
-        when text = scan(/</)
+        when scan(/</)
           emit :LT
 
-        when text = scan(/&&/)
+        when scan(/&&/)
           emit :AND
 
-        when text = scan(/=/)
+        when scan(/=/)
           emit :EQUAL
 
         when text = scan(/[a-zA-Z_][a-zA-Z0-9_]*/)
@@ -139,12 +145,6 @@ module MiniJava
 
         when text = scan(/0(?![1-9]+)|[1-9][0-9]*/)
           emit :INT_LITERAL, Integer(text)
-
-        when text = scan(/[ \r\n\t\f]+/)
-          nil
-
-        when text = scan(/\/\*.*?\*\/|\/\*\*+\/|\/\/[^\r\n]*/)
-          nil
 
         when text = scan(/./)
           raise ParseError, "Illegal character '#{text}' on line #{@line}"
