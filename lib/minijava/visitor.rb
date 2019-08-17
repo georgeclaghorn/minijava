a444d74c9bc56d4a6087c4b0b90b80f6be4785b2
George Claghorn <george@basecamp.com>
2019-08-16 20:50:40 -0400

    Do, there is no try

--- lib/minijava/visitor.rb
+++ lib/minijava/visitor.rb
@@ -1,10 +1,9 @@
 require "active_support/core_ext/string/inflections"
-require "active_support/core_ext/object/try"
 
 module MiniJava
   class Visitor
     def visit(visitable)
-      try "visit_#{visitable.class.name.demodulize.underscore}", visitable
+      send "visit_#{visitable.class.name.demodulize.underscore}", visitable
     end
 
     def visit_all(visitables)
