f8f1996fc9afa3dab8aa87614320afcfb921f940
George Claghorn <george@basecamp.com>
2019-08-17 13:45:11 -0400

    Reorder

--- test/unit/lexer_test.rb
+++ test/unit/lexer_test.rb
@@ -1,51 +1,6 @@
 require "test_helper"
 
 class MiniJava::LexerTest < Minitest::Test
-  def test_counting_lines_through_code
-    lexer = MiniJava::Lexer.new("foo;\nbar;\rbaz;\r\nglorp;")
-
-    %w[ foo bar baz glorp ].each_with_index do |identifier, index|
-      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
-      assert_equal [ :SEMICOLON, nil ], lexer.next_token
-      assert_equal index + 1, lexer.line
-    end
-  end
-
-  def test_counting_lines_through_comments
-    lexer = MiniJava::Lexer.new(<<~JAVA)
-      foo;
-      bar;
-      /**
-       * baz
-       * glorp\r
-
-       */
-      quux;
-    JAVA
-
-    { "foo" => 1, "bar" => 2, "quux" => 8 }.each do |identifier, line|
-      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
-      assert_equal [ :SEMICOLON, nil ], lexer.next_token
-      assert_equal line, lexer.line
-    end
-  end
-
-  def test_ignoring_single_line_comments
-    lexer = MiniJava::Lexer.new(<<~JAVA)
-      foo;
-      bar;
-      // baz
-      glorp;
-    JAVA
-
-    %w[ foo bar glorp ].each do |identifier|
-      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
-      assert_equal [ :SEMICOLON, nil ], lexer.next_token
-    end
-
-    assert_equal 4, lexer.line
-  end
-
   def test_scanning_keywords
     lexer = MiniJava::Lexer.new(<<~JAVA)
       boolean
@@ -106,5 +61,50 @@ class MiniJava::LexerTest < Minitest::Test
 
     error = assert_raises(MiniJava::SyntaxError) { lexer.next_token }
     assert_equal "Illegal character '^' on line 4", error.message
+  end
+
+  def test_ignoring_single_line_comments
+    lexer = MiniJava::Lexer.new(<<~JAVA)
+      foo;
+      bar;
+      // baz
+      glorp;
+    JAVA
+
+    %w[ foo bar glorp ].each do |identifier|
+      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
+      assert_equal [ :SEMICOLON, nil ], lexer.next_token
+    end
+
+    assert_equal 4, lexer.line
+  end
+
+  def test_counting_lines_through_code
+    lexer = MiniJava::Lexer.new("foo;\nbar;\rbaz;\r\nglorp;")
+
+    %w[ foo bar baz glorp ].each_with_index do |identifier, index|
+      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
+      assert_equal [ :SEMICOLON, nil ], lexer.next_token
+      assert_equal index + 1, lexer.line
+    end
+  end
+
+  def test_counting_lines_through_multiline_comments
+    lexer = MiniJava::Lexer.new(<<~JAVA)
+      foo;
+      bar;
+      /**
+       * baz
+       * glorp\r
+
+       */
+      quux;
+    JAVA
+
+    { "foo" => 1, "bar" => 2, "quux" => 8 }.each do |identifier, line|
+      assert_equal [ :IDENTIFIER, identifier ], lexer.next_token
+      assert_equal [ :SEMICOLON, nil ], lexer.next_token
+      assert_equal line, lexer.line
+    end
   end
 end
--- test/unit/parser_test.rb
+++ test/unit/parser_test.rb
@@ -2,6 +2,78 @@ require "test_helper"
 require "active_support/core_ext/array/access"
 
 class MiniJava::ParserTest < Minitest::Test
+  def test_parsing_decimal_integer_literals
+    program = parse <<~PROGRAM
+      class Foo {
+        public static void main(String[] args) {
+          System.out.println(0 + 100 + 123 + 37);
+        }
+      }
+    PROGRAM
+
+    integer_literals = program.select(MiniJava::Syntax::IntegerLiteral)
+    assert_equal 0, integer_literals.first.value
+    assert_equal 100, integer_literals.second.value
+    assert_equal 123, integer_literals.third.value
+    assert_equal 37, integer_literals.fourth.value
+  end
+
+  def test_parsing_array_element_assignments
+    program = parse <<~PROGRAM
+      class Foo {
+        public static void main(String[] args) {
+          System.out.println(new NumberPicker().pick());
+        }
+      }
+
+      class NumberPicker {
+        int[] numbers;
+
+        public int pick() {
+          numbers = new int[1];
+          numbers[0] = 1;
+          return numbers[0];
+        }
+      }
+    PROGRAM
+
+    assignments = program.select(MiniJava::Syntax::Assignment)
+    assert_equal 2, assignments.count
+
+    assignment = assignments.second
+
+    assert_kind_of MiniJava::Syntax::ArraySubscript, assignment.left
+    assert_equal "numbers", assignment.left.array.name
+    assert_equal 0, assignment.left.index.value
+
+    assert_kind_of MiniJava::Syntax::IntegerLiteral, assignment.right
+    assert_equal 1, assignment.right.value
+  end
+
+  def test_parsing_array_length
+    program = parse <<~PROGRAM
+      class Foo {
+        public static void main(String[] args) {
+          System.out.println(new NumberPicker().pick());
+        }
+      }
+
+      class NumberPicker {
+        int[] numbers;
+
+        public int pick() {
+          numbers = new int[1];
+          numbers[0] = numbers.length;
+          return numbers[0];
+        }
+      }
+    PROGRAM
+
+    lengths = program.select(MiniJava::Syntax::ArrayLength)
+    assert lengths.one?
+    assert_equal "numbers", lengths.first.array.name
+  end
+
   def test_detecting_invalid_statement_syntax
     program = nil
 
@@ -47,8 +119,9 @@ class MiniJava::ParserTest < Minitest::Test
 
     assert_equal "Parse error on line 8", output.strip
 
-    assert_equal "Bar", program.class_declarations.first.name.to_s
-    assert_kind_of MiniJava::Syntax::InvalidVariableDeclaration, program.class_declarations.first.variable_declarations.first
+    class_declaration = program.class_declarations.first
+    assert_equal "Bar", class_declaration.name.to_s
+    assert_kind_of MiniJava::Syntax::InvalidVariableDeclaration, class_declaration.variable_declarations.first
 
     assert_equal "Quux", program.class_declarations.second.name.to_s
   end
@@ -203,79 +276,11 @@ class MiniJava::ParserTest < Minitest::Test
     assert_equal "Parse error on line 8", output.strip
   end
 
-  def test_parsing_decimal_integer_literals
-    program = parse <<~PROGRAM
-      class Foo {
-        public static void main(String[] args) {
-          System.out.println(0 + 100 + 123 + 37);
-        }
-      }
-    PROGRAM
-
-    integer_literals = program.select(MiniJava::Syntax::IntegerLiteral)
-    assert_equal 0, integer_literals.first.value
-    assert_equal 100, integer_literals.second.value
-    assert_equal 123, integer_literals.third.value
-    assert_equal 37, integer_literals.fourth.value
-  end
-
-  def test_parsing_array_element_assignments
-    program = parse <<~PROGRAM
-      class Foo {
-        public static void main(String[] args) {
-          System.out.println(new NumberPicker().pick());
-        }
-      }
-
-      class NumberPicker {
-        int[] numbers;
-
-        public int pick() {
-          numbers = new int[1];
-          numbers[0] = 1;
-          return numbers[0];
-        }
-      }
-    PROGRAM
-
-    assignments = program.select(MiniJava::Syntax::Assignment)
-    assert_equal 2, assignments.count
-
-    assignment = assignments.second
-
-    assert_kind_of MiniJava::Syntax::ArraySubscript, assignment.left
-    assert_equal "numbers", assignment.left.array.name
-    assert_equal 0, assignment.left.index.value
-
-    assert_kind_of MiniJava::Syntax::IntegerLiteral, assignment.right
-    assert_equal 1, assignment.right.value
-  end
-
-  def test_parsing_array_length
-    program = parse <<~PROGRAM
-      class Foo {
-        public static void main(String[] args) {
-          System.out.println(new NumberPicker().pick());
-        }
-      }
-
-      class NumberPicker {
-        int[] numbers;
-
-        public int pick() {
-          numbers = new int[1];
-          numbers[0] = numbers.length;
-          return numbers[0];
-        }
-      }
-    PROGRAM
-
-    lengths = program.select(MiniJava::Syntax::ArrayLength)
-    assert lengths.one?
-    assert_equal "numbers", lengths.first.array.name
-  end
-
   private
+    def parse(source)
+      MiniJava::Parser.program_from(source)
+    end
+
     def capture(stream)
       destination = Tempfile.new
       origin = stream.dup
@@ -287,9 +292,5 @@ class MiniJava::ParserTest < Minitest::Test
     ensure
       destination.close!
       stream.reopen(origin)
-    end
-
-    def parse(source)
-      MiniJava::Parser.program_from(source)
     end
 end
