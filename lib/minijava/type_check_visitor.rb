e0ae0fe2e1354d9c97d81bbd5a5deed22e0edb55
George Claghorn <george@basecamp.com>
2019-08-30 16:41:21 -0400

    Add type convenience methods

--- lib/minijava/type_check_visitor.rb
+++ lib/minijava/type_check_visitor.rb
@@ -55,18 +55,18 @@ module MiniJava
     end
 
     def visit_if_statement(statement)
-      assert_type_of "boolean", statement.condition
+      assert_type_of boolean, statement.condition
       visit statement.affirmative
       visit statement.negative
     end
 
     def visit_while_statement(statement)
-      assert_type_of "boolean", statement.condition
+      assert_type_of boolean, statement.condition
       visit statement.substatement
     end
 
     def visit_print_statement(statement)
-      assert_type_of "int", statement.expression
+      assert_type_of integer, statement.expression
     end
 
     def visit_variable_assignment(assignment)
@@ -74,29 +74,29 @@ module MiniJava
     end
 
     def visit_array_element_assignment(assignment)
-      assert_equal "int[]", scope.variable_type_by_name!(assignment.array),
+      assert_equal array, scope.variable_type_by_name!(assignment.array),
         "Incompatible types: expected %<expected>s, got %<actual>s"
 
-      assert_type_of "int", assignment.index
-      assert_type_of "int", assignment.value
+      assert_type_of integer, assignment.index
+      assert_type_of integer, assignment.value
     end
 
 
     def visit_not(operation)
-      assert_type_of "boolean", operation.expression
-      MiniJava::Syntax::BooleanType.instance
+      assert_type_of boolean, operation.expression
+      boolean
     end
 
     def visit_and(operation)
-      assert_type_of "boolean", operation.left
-      assert_type_of "boolean", operation.right
-      MiniJava::Syntax::BooleanType.instance
+      assert_type_of boolean, operation.left
+      assert_type_of boolean, operation.right
+      boolean
     end
 
     def visit_binary_arithmetic_operation(operation)
-      assert_type_of "int", operation.left
-      assert_type_of "int", operation.right
-      MiniJava::Syntax::IntegerType.instance
+      assert_type_of integer, operation.left
+      assert_type_of integer, operation.right
+      integer
     end
 
     alias_method :visit_less_than, :visit_binary_arithmetic_operation
@@ -109,14 +109,14 @@ module MiniJava
     end
 
     def visit_array_access(access)
-      assert_type_of "int[]", access.array
-      assert_type_of "int", access.index
-      MiniJava::Syntax::IntegerType.instance
+      assert_type_of array, access.array
+      assert_type_of integer, access.index
+      integer
     end
 
     def visit_array_length(length)
-      assert_type_of "int[]", length.array
-      MiniJava::Syntax::IntegerType.instance
+      assert_type_of array, length.array
+      integer
     end
 
     def visit_method_invocation(invocation)
@@ -133,23 +133,23 @@ module MiniJava
     end
 
     def visit_new_array(expression)
-      MiniJava::Syntax::ArrayType.instance
+      array
     end
 
     def visit_new_object(expression)
-      MiniJava::Syntax::ObjectType.new expression.class_name
+      object expression.class_name
     end
 
     def visit_integer_literal(literal)
-      MiniJava::Syntax::IntegerType.instance
+      integer
     end
 
     def visit_true_literal(literal)
-      MiniJava::Syntax::BooleanType.instance
+      boolean
     end
 
     def visit_false_literal(literal)
-      MiniJava::Syntax::BooleanType.instance
+      boolean
     end
 
     private
@@ -164,12 +164,28 @@ module MiniJava
       end
 
 
+      def boolean
+        MiniJava::Syntax::BooleanType.instance
+      end
+
+      def integer
+        MiniJava::Syntax::IntegerType.instance
+      end
+
+      def array
+        MiniJava::Syntax::ArrayType.instance
+      end
+
+      def object(class_name)
+        MiniJava::Syntax::ObjectType.new(class_name)
+      end
+
       def assert_type_of(expected, visitable, message = "Incompatible types: expected %<expected>s, got %<actual>s")
         assert_equal expected, visit(visitable), message
       end
 
       def assert_equal(expected, actual, message = "Expected %<expected>s, got %<actual>s")
-        unless actual == expected || actual.to_s == expected.to_s
+        unless actual == expected
           raise TypeError, sprintf(message, expected: expected, actual: actual)
         end
       end
